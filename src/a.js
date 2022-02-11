934ba28c2ab63f8bf99c736274ee8491c78a9ee5
Hehly <1830904228@qq.com>
2022-02-12 00:41:28 +0800

    webpack-dev-server config webpack.config.js

--- src/a.js
+++ src/a.js
@@ -1,10 +1,10 @@
-console.log("我是a");
+console.log("我是a -webpack-dev-server");
 
 
-// commontJS 规范
-module.exports ={
-    name:"aa"
-}
+// commontJS 规范 导出
+// module.exports ={
+//     name:"aa"
+// }
 
 // es6规范
 export default {
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,15 @@ module.exports = {
   output: {
     // path: path.resolve(__dirname, "dist"), // 绝对路径 两种写法 path 指定
     path: path.join(__dirname, "dist"),
-    filename: "[name].[hash:8].js",
+    // filename: "[name].[hash:8].js",
+    filename: "main.js",
   },
-  watch:true // 开启监视 方案二
+  watch:true, // 开启监视 方案二
+  devServer:{
+      open:true,
+      port:8080,
+      compress:true,
+      hot:true,
+      contentBase:"./public"
+  }
 };
