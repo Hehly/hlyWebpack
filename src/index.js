4834c7c5181f4386b5da141f0050079a19720b5d
Hehly <1830904228@qq.com>
2022-02-12 01:24:41 +0800

    处理css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let a = require("./a.js");
 // commonJS规范
+import './css/index.css';
 
 console.log(a,"我是引用a");
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development", // 模式 development production
@@ -9,12 +10,26 @@ module.exports = {
     // filename: "[name].[hash:8].js",
     filename: "main.js",
   },
-  watch:true, // 开启监视 方案二
-  devServer:{
-      open:true,
-      port:8080,
-      compress:true,
-      hot:true,
-      contentBase:"./public"
-  }
+  module: {
+    rules: [
+      {
+        test: /\.css$/g, // 解析css
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins:[
+      // 插入到指定的位置
+    new htmlWebpackPlugin({
+        template:"./public/index.html",
+        filename:"index.html"
+    })
+  ],
+//   watch: true, // 开启监视 方案二
+  devServer: {
+    open: true,
+    port: 8080,
+    compress: true,
+    hot: true,
+  },
 };
