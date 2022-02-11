6f6767520ba9a845b34d03cb3a5dec0cfa9ccd36
Hehly <1830904228@qq.com>
2022-02-11 23:40:48 +0800

    多文件打包单个文件

---
+++ src/a.js
@@ -0,0 +1,12 @@
+console.log("我是a");
+
+
+// commontJS 规范
+module.exports ={
+    name:"aa"
+}
+
+// es6规范
+export default {
+    name:"aa"
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+let a = require("./a.js");
+// commonJS规范
+
+console.log(a,"我是引用a");
+
 console.log("webpack");
